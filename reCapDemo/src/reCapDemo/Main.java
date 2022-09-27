373337cb60e0fce9743291cf92f64bb80748f446
yozmen <yozmen@gmail.com>
2022-09-27 22:18:13 +0300

    c

--- reCapDemo/src/reCapDemo/Main.java
+++ reCapDemo/src/reCapDemo/Main.java
@@ -3,7 +3,7 @@ package reCapDemo;
 public class Main {
     public static void main(String[] args) {
         DortIslem dortIslem = new DortIslem();
-        int result = dortIslem.add(6, 4);
+        int result = dortIslem.add(6, 3);
         System.out.println(result);
     }
 }
