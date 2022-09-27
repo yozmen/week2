22178bae2746f5ee057b79f796f6b8abceb0ea1d
yozmen <yozmen@gmail.com>
2022-09-27 22:09:43 +0300

    c

--- inheritanceDemo/src/inheritanceDemo/Main.java
+++ inheritanceDemo/src/inheritanceDemo/Main.java
@@ -3,7 +3,7 @@ package inheritanceDemo;
 public class Main {
     public static void main(String[] args) {
         KrediUI krediUI=new KrediUI();
-        krediUI.krediHesapla(new OgretmenKrediManager());
+        krediUI.krediHesapla(new TarimKrediManager());
     }
 
 }
